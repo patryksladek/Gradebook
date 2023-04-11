a3af351e60dd3473babb27ffa29233a9bfb47c7c
Patryk Sładek <p.sladek@infomex.pl>
2023-04-11 14:29:43 +0200

    4_5

---
+++ Gradebook.Domain/Abstractions/IStudentRepository.cs
@@ -0,0 +1,14 @@
+﻿using Gradebook.Domain.Entities;
+
+namespace Gradebook.Domain.Abstractions;
+
+public interface IStudentRepository
+{
+    Task<IEnumerable<Student>> GetAllAsync(CancellationToken cancellationToken = default);
+    Task<Student> GetByIdAsync(int id, CancellationToken cancellationToken = default);
+    Task<Student> GetByEmailAsync(string email, CancellationToken cancellationToken = default);
+    Task<bool> IsAlreadyExistAsync(string email, CancellationToken cancellationToken = default);
+    void Add(Student student);
+    void Update(Student student);
+    void Delete(Student student);
+}
---
+++ Gradebook.Infrastructure/Repositories/StudentRepository.cs
@@ -0,0 +1,42 @@
+﻿using Gradebook.Domain.Abstractions;
+using Gradebook.Domain.Entities;
+
+namespace Gradebook.Infrastructure.Repositories;
+
+internal class StudentRepository : IStudentRepository
+{
+    public Task<IEnumerable<Student>> GetAllAsync(CancellationToken cancellationToken = default)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<Student> GetByIdAsync(int id, CancellationToken cancellationToken = default)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<Student> GetByEmailAsync(string email, CancellationToken cancellationToken = default)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<bool> IsAlreadyExistAsync(string email, CancellationToken cancellationToken = default)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Add(Student student)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Update(Student student)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Delete(Student student)
+    {
+        throw new NotImplementedException();
+    }
+}
