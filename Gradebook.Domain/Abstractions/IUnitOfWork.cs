fae3ccecc9e16231441776587eb3aa206d98c182
Patryk Sładek <p.sladek@infomex.pl>
2023-04-11 14:53:05 +0200

    4_6

---
+++ Gradebook.Domain/Abstractions/IUnitOfWork.cs
@@ -0,0 +1,6 @@
+﻿namespace Gradebook.Domain.Abstractions;
+
+public interface IUnitOfWork
+{
+    Task SaveChangesAsync(CancellationToken cancellationToken = default);
+}
---
+++ Gradebook.Infrastructure/UnitOfWork.cs
@@ -0,0 +1,12 @@
+﻿using Gradebook.Domain.Abstractions;
+using Gradebook.Domain.Entities;
+
+namespace Gradebook.Infrastructure;
+
+internal class UnitOfWork : IUnitOfWork
+{
+    public Task SaveChangesAsync(CancellationToken cancellationToken = default)
+    {
+        throw new NotImplementedException();
+    }
+}
