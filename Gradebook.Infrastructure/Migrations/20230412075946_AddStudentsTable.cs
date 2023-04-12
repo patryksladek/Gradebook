32810b5941e8b6b9dfadc509b5797a653613ee03
Patryk Sładek <p.sladek@infomex.pl>
2023-04-12 11:17:16 +0200

    5_5

---
+++ Gradebook.Infrastructure/Migrations/20230412075946_AddStudentsTable.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Gradebook.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddStudentsTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.EnsureSchema(
+                name: "gradebook");
+
+            migrationBuilder.CreateTable(
+                name: "Students",
+                schema: "gradebook",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    FirstName = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    LastName = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    Email = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    DateOfBirth = table.Column<DateTime>(type: "date", nullable: false),
+                    YearEnrolled = table.Column<int>(type: "int", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "datetime2(0)", nullable: false),
+                    ModifiedAt = table.Column<DateTime>(type: "datetime2(0)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Students", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Students_Email",
+                schema: "gradebook",
+                table: "Students",
+                column: "Email",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Students",
+                schema: "gradebook");
+        }
+    }
+}
