83bb58368cf7124a90fb2f842b407a087873fc76
Patryk Sładek <p.sladek@infomex.pl>
2023-04-12 12:34:00 +0200

    6_1

--- Gradebook.Api/Program.cs
+++ Gradebook.Api/Program.cs
@@ -1,3 +1,4 @@
+using Gradebook.Application;
 using Gradebook.Infrastructure;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -5,6 +6,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddInfrastructure(builder.Configuration);
+builder.Services.AddApplication();
 
 //builder.Services.AddControllers();
 //// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
---
+++ Gradebook.Application/Extensions.cs
@@ -0,0 +1,16 @@
+﻿using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+
+namespace Gradebook.Application;
+
+public static class Extensions
+{
+    public static IServiceCollection AddApplication(this IServiceCollection services)
+    {
+        var executingAssembly = Assembly.GetExecutingAssembly();
+        services.AddMediatR(executingAssembly);
+
+        return services;
+    }
+}
